a7693505124c2099a027207e58453155fb3b24ef
nikos_liakas <liakasn@gmail.com>
2025-01-13 15:05:46 +0000

    Generics - chp2 classes

--- week1/src/main/GenericClasses.java
+++ week1/src/main/GenericClasses.java
@@ -11,7 +11,18 @@ public class GenericClasses{
         * NB - with any variable declaration p1 doesn't hold a Person Object, just that it will contain
         *       a Person with a reference to an Employee object
         * */
-        Person<String> p1 = new Person<>();
+        Person<Employee> e1 = new Person<>();
+        Person<Contact> c1 = new Person<>();
+
+        Employee emp1 = new Employee("John", "Slattery", "123456");
+        Contact con1 = new Contact("Mrs", "Peggy", "Fisher", "717-555-2131");
+
+        e1.setPerson(emp1);
+        c1.setPerson(con1);
+
+        System.out.println(e1.getPerson().toString());
+        System.out.println(c1.getPerson().toString());
+
     }
 }
 
