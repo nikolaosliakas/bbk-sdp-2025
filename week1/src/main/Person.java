16f23ecdf9ea17d12dd5b2f1251791002e2790cb
nikos_liakas <liakasn@gmail.com>
2025-01-13 15:00:01 +0000

    Generics - chp2 classes

--- week1/src/main/Person.java
+++ week1/src/main/Person.java
@@ -1,5 +1,13 @@
 package main;
 
 public class Person<E>{
-
+    public E e;
+    public void setPerson(E e)
+    {
+        this.e = e;
+    }
+    public E getPerson()
+    {
+        return e;
+    }
 }
